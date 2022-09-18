c67b9d9c6fa505079c9be726949304c58ccb518a
Arka102202 <111365238+Arka102202@users.noreply.github.com>
2022-09-18 15:00:13 +0530

    committing the modifies test file

--- test/Test.java
+++ test/Test.java
@@ -13,23 +13,25 @@ public class Test {
         CredService cs = new CredService();
 
         System.out.println("Welcome to " + cs.companyName);
-        String firstName, lastName, department;
+        String firstName, lastName, option;
         System.out.print("Please enter your First name: ");
         firstName = sc.nextLine();
         System.out.println(firstName);
         System.out.print("Please enter your Last name: ");
         lastName = sc.nextLine();
         System.out.println(lastName);
+        String[] departments = new String[]{"tech","adm","hr","lg"};
         System.out.print("Please enter the department from the following list:\n" +
                 "\t1. Technical\n" +
                 "\t2. Admin\n" +
                 "\t3. Human Resource\n" +
                 "\t4. Legal\n" +
                 "Your choice: ");
-        department = sc.nextLine();
+        System.out.println("(Please use the serial number to enter your choice)");
+        option = sc.nextLine();
         Employee e = new Employee(firstName, lastName);
 
-        cs.showCredentials(e,department);
+        cs.showCredentials(e,departments[Integer.parseInt(option)-1]);
     }
 
 
